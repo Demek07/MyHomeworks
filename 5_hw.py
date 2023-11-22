0d94a55ef5c25a3fcfd6534bb9b7c964195192e1
Demek07 <136734449+Demek07@users.noreply.github.com>
2023-11-22 14:45:42 +0300

    Расшифровка послания с использованием вложенного цикла for

---
+++ 5_hw.py
@@ -0,0 +1,22 @@
+# Секретное послание
+secret_letter = [['DFВsjl24sfFFяВАДОd24fssflj234'], ['asdfFп234рFFdо24с$#afdFFтasfо'],
+['оafбasdf%^о^FFжа$#af243ю'],['afпFsfайFтFsfо13н'],
+['fн13Fа1234де123юsdсsfь'], ['чFFтF#Fsfsdf$$о'],
+['и$ #sfF'], ['вSFSDам'],['пSFоsfнрSDFаSFвSDF$иFFтsfaсSFя'],
+['FFэasdfтDFsfоasdfFт'], ['FяDSFзFFsыSfкFFf']]
+
+# Список с маленькими русскими буквами
+small_rus = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и',
+'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф',
+'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
+res_message = ''
+
+for item in secret_letter:
+    for word in item:
+        for symbol in word:
+            if symbol in small_rus:
+                res_message += symbol
+        res_message += ' '
+print(res_message.title())
+input('Для выхода из программы нажмите Enter')
+
